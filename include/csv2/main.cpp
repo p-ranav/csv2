b1b0d3bfe857ef01263104df19022a6b8fec7e7a
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-04-17 14:40:52 -0700

    Minor update

--- include/csv2/main.cpp
+++ include/csv2/main.cpp
@@ -4,12 +4,12 @@ int main(int argc, char **argv) {
   csv2::reader csv(std::string{argv[1]}, ',');
   csv2::row_t row;
   while(csv.read_row(row)) {
-    for (auto& [k, v]: row)
-        std::cout << k << ":" << v << "\n";
-    std::cout << "\n";
+    // for (auto& [k, v]: row)
+    //     std::cout << k << ":" << v << "\n";
+    // std::cout << "\n";
   }
-  std::cout << "Loaded " << csv.rows() << "x" << csv.cols() << " entries\n";
-  auto header = csv.header();
-  for (auto& h: header)
-    std::cout << h << "; ";
+  // std::cout << "Loaded " << csv.rows() << "x" << csv.cols() << " entries\n";
+  // auto header = csv.header();
+  // for (auto& h: header)
+  //   std::cout << h << "; ";
 }
