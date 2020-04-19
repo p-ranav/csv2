cc7075fcebe0cd70fa7dd493aa076436881262b0
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-04-18 22:04:27 -0500

    Clang format

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -1,32 +1,26 @@
+#include <chrono>
 #include <csv2/reader.hpp>
 #include <iostream>
-#include <chrono>
 using namespace csv2;
 
 int main(int argc, char **argv) {
 
   auto print_exec_time = [](auto start, auto stop) {
-			   auto duration_us =
-			     std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-			   auto duration_ms =
-			     std::chrono::duration_cast<std::chrono::milliseconds>(stop - start); 
-			   auto duration_s =
-			     std::chrono::duration_cast<std::chrono::seconds>(stop - start);   
-			   
-			   std::cout << duration_us.count() << " us | "
-				     << duration_ms.count() << " ms | "
-				     << duration_s.count() << " s\n";
-			 };
+    auto duration_us = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    auto duration_s = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+
+    std::cout << duration_us.count() << " us | " << duration_ms.count() << " ms | "
+              << duration_s.count() << " s\n";
+  };
 
   auto start = std::chrono::high_resolution_clock::now();
-  
+
   Reader csv{
-    option::Filename{std::string(argv[1])},
-    option::Delimiter{','},
-    option::SkipInitialSpace{true},
-    option::TrimCharacters{std::vector<char>{'\n', '\r'}},
-    option::TrimPolicy{Trim::trailing}
-    // ...
+      option::Filename{std::string(argv[1])}, option::Delimiter{','},
+      option::SkipInitialSpace{true}, option::TrimCharacters{std::vector<char>{'\n', '\r'}},
+      option::TrimPolicy{Trim::trailing}
+      // ...
   };
 
   auto stop = std::chrono::high_resolution_clock::now();
