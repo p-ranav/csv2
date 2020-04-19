d927bbb8c069af28fc82fc15f3af9bba4836bde1
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-04-18 20:22:41 -0500

    Updated benchmark program

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -1,8 +1,12 @@
 #include <csv2/reader.hpp>
+#include <iostream>
+#include <chrono>
 using namespace csv2;
 
 int main(int argc, char **argv) {
 
+  auto start = std::chrono::high_resolution_clock::now();
+  
   Reader csv{
     option::Filename{std::string(argv[1])},
     option::Delimiter{','},
@@ -16,4 +20,16 @@ int main(int argc, char **argv) {
   while (csv.read_row(next)) {
     // Iterate over all rows in CSV
   }
+
+  // Get ending timepoint 
+  auto stop = std::chrono::high_resolution_clock::now(); 
+  
+  // Get duration. Substart timepoints to  
+  // get durarion. To cast it to proper unit 
+  // use duration cast method 
+  auto duration =
+    std::chrono::duration_cast<std::chrono::microseconds>(stop - start); 
+  
+  std::cout << "Execution Time: " << duration.count() << " us\n"; 
+  
 }
